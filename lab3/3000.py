62a539090b1aaaae5a7dc7da5b816ca89c96c47b
ohnoitsmo <justin1998mo@gmail.com>
2019-04-29 22:27:02 -0700

    lab3

--- lab3/3000.py
+++ lab3/3000.py
@@ -1,6 +1,6 @@
 import random
 
 file = open("testfile.txt", "w")
-for i in range(0, 3000):
-	file.write(str(random.randint(0, 3000))+" ")
+for i in range(0, 3000000):
+	file.write(str(random.randint(0, 3000000))+" ")
 file.close()
